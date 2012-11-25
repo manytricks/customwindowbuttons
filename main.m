1770257dbafd01b616bf2d1c077caa1f8593b379
Peter Maurer <peter@manytricks.com>
2012-11-25 22:58:51 +0100

    Removed useless copyright info.

--- main.m
+++ main.m
@@ -1,14 +1,6 @@
-//
-//  main.m
-//  Standard Window Button Test
-//
-//  Created by Peter Maurer on 24.02.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc,  (const char **) argv);
+
+int main(int argc, char *argv[]) {
+    return NSApplicationMain(argc, (const char **) argv);
 }
